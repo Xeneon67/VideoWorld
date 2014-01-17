e56efb3ee14dcc50d7194d27ddcab972a6154c08
Guillaume <guigbeaulieu@gmail;com>
2014-01-17 17:16:06 +0100

    test

---
+++ app/src/main/java/com/videoword/customviews/VideoPlayerView.java
@@ -0,0 +1,15 @@
+package com.videoword.customviews;
+
+import android.content.Context;
+import android.widget.VideoView;
+
+/**
+ * Created by Guillaume on 17/01/2014.
+ */
+public class VideoPlayerView extends VideoView{
+    public VideoPlayerView(Context context) {
+        super(context);
+    }
+
+
+}
